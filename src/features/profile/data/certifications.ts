bfc0b25e3a981302a5ab5c3cd06960bc3a05a092
Melek HADDAR <haddarmalek23@gmail.com>
2025-07-15 12:27:54 +0100

    Update certifications

--- src/features/profile/data/certifications.ts
+++ src/features/profile/data/certifications.ts
@@ -2,144 +2,76 @@ import type { Certification } from "../types/certifications";
 
 export const CERTIFICATIONS: Certification[] = [
   {
-    title: "Certificate of Trademark Registration No. 543682",
-    issuer: "Intellectual Property Office of Viet Nam",
-    issuerLogoURL:
-      "https://assets.chanhdai.com/images/companies/ipvietnam.webp",
-    issueDate: "2025-05-08",
-    credentialID: "543682",
-    credentialURL:
-      "https://drive.google.com/file/d/1x7YzlK1kyz16h28ux9k3KAwnZFAabsvq/view?usp=sharing",
-  },
-  {
     title: "Next.js SEO Fundamentals",
     issuer: "Vercel",
     issuerIconName: "vercel",
-    issueDate: "2025-04-26",
-    credentialID: "seo-47463-1745634751873",
+    issuerLogoURL:
+      "https://assets.vercel.com/image/upload/front/favicon/vercel/180x180.png",
+    issueDate: "2025-07-01",
+    credentialID: "seo",
     credentialURL:
-      "https://nextjs.org/learn/certificate?course=seo&user=47463&certId=seo-47463-1745634751873",
+      "https://nextjs.org/learn/certificate?course=seo&user=80451&certId=seo-80451-1752574495166",
   },
   {
     title: "Next.js App Router Fundamentals",
     issuer: "Vercel",
     issuerIconName: "vercel",
-    issueDate: "2025-04-26",
-    credentialID: "dashboard-app-47463-1745633258744",
+    issuerLogoURL:
+      "https://assets.vercel.com/image/upload/front/favicon/vercel/180x180.png",
+    issueDate: "2025-07-01",
+    credentialID: "dashboard-app",
     credentialURL:
-      "https://nextjs.org/learn/certificate?course=dashboard-app&user=47463&certId=dashboard-app-47463-1745633258744",
+      "https://nextjs.org/learn/certificate?course=dashboard-app&user=80451&certId=dashboard-app-80451-1752571986854",
+  },
+  {
+    title: "Next.js Pages Router Fundamentals",
+    issuer: "Vercel",
+    issuerIconName: "vercel",
+    issuerLogoURL:
+      "https://assets.vercel.com/image/upload/front/favicon/vercel/180x180.png",
+    issueDate: "2025-07-01",
+    credentialID: "dashboard-app",
+    credentialURL:
+      "https://nextjs.org/learn/certificate?course=pages-router&user=80451&certId=pages-router-80451-1752574000977",
   },
   {
     title: "React Foundations for Next.js",
     issuer: "Vercel",
     issuerIconName: "vercel",
-    issueDate: "2025-04-26",
-    credentialID: "react-foundations-47463-1745634245158",
-    credentialURL:
-      "https://nextjs.org/learn/certificate?course=react-foundations&user=47463&certId=react-foundations-47463-1745634245158",
-  },
-  {
-    title: "Certificate of Copyright Registration No. 0040/2025/QTG",
-    issuer: "Copyright Office of Viet Nam",
-    issuerLogoURL: "https://assets.chanhdai.com/images/companies/cov.webp",
-    issueDate: "2025-01-02",
-    credentialID: "0040/2025/QTG",
-    credentialURL:
-      "https://drive.google.com/file/d/1kVBByVLlYPyyUJvxKga670wvVPNmZ2nV/view?usp=sharing",
-  },
-  {
-    title: "Vietnamese Standardized Test of English Proficiency (CEFR B1)",
-    issuer: "USSH-VNUHCM",
     issuerLogoURL:
-      "https://assets.chanhdai.com/images/companies/ussh-vnuhcm.webp",
-    issueDate: "2024-12-17",
-    credentialID: "QH58202305187",
+      "https://assets.vercel.com/image/upload/front/favicon/vercel/180x180.png",
+    issueDate: "2025-07-01",
+    credentialID: "react-foundations",
     credentialURL:
-      "https://vbcc.hcmussh.edu.vn/?type=11&certificateType=7&no=QH58202305187&fullName=Nguy%E1%BB%85n+Ch%C3%A1nh+%C4%90%E1%BA%A1i&dob=2000-08-14",
+      "https://nextjs.org/learn/certificate?course=react-foundations&user=80451&certId=react-foundations-80451-1752571339788",
   },
   {
-    title: "Certificate of Copyright Registration No. 7994/2024/QTG",
-    issuer: "Copyright Office of Viet Nam",
-    issuerLogoURL: "https://assets.chanhdai.com/images/companies/cov.webp",
-    issueDate: "2024-09-18",
-    credentialID: "7994/2024/QTG",
+    title: "Python for Data Science",
+    issuer: "IBM",
+    issuerIconName: "ibm",
+    issuerLogoURL:
+      "https://upload.wikimedia.org/wikipedia/commons/5/51/IBM_logo.svg",
+    issueDate: "2024-10-01",
+    credentialID: "course-v1:CognitiveClass+PY0101EN+v3",
     credentialURL:
-      "https://drive.google.com/file/d/1otjV4GNOLFj4JD2tHWLuZpUIVyITYNUy/view?usp=sharing",
+      "https://courses.cognitiveclass.ai/certificates/f178b80d9e484206ad6bb1c3abf909c3",
   },
   {
-    title: "Engaging in Persuasive and Credible Communication",
-    issuer: "National University of Singapore",
-    issuerIconName: "coursera",
-    issueDate: "2022-09-13",
-    credentialID: "C6DHB9A5XQHV",
-    credentialURL: "https://coursera.org/verify/C6DHB9A5XQHV",
-  },
-  {
-    title: "Introduction to Databases",
-    issuer: "Meta",
-    issuerIconName: "meta",
-    issueDate: "2022-08-28",
-    credentialID: "YV5VQ5MXZ5YH",
-    credentialURL: "https://coursera.org/verify/YV5VQ5MXZ5YH",
-  },
-  {
-    title: "Solving Problems with Creative and Critical Thinking",
-    issuer: "IBM Skills Network",
-    issuerIconName: "coursera",
-    issueDate: "2022-08-27",
-    credentialID: "4UKZQJGM2932",
-    credentialURL: "https://coursera.org/verify/4UKZQJGM2932",
-  },
-  {
-    title: "Digital Skills: Social Media",
-    issuer: "Accenture",
-    issuerIconName: "accenture",
-    issueDate: "2022-08-27",
-    credentialID: "pc4345i",
-    credentialURL: "https://www.futurelearn.com/certificates/pc4345i",
-  },
-  {
-    title: "The Fundamentals of Digital Marketing",
-    issuer: "Google Digital Garage",
-    issuerIconName: "google",
-    issueDate: "2022-08-22",
-    credentialID: "3T8-F9N-LKQ",
-    credentialURL:
-      "https://drive.google.com/file/d/14laAMWmVKhCfgKwVEgJyWse7o0sZDD4N/view?usp=sharing",
-  },
-  {
-    title: "Microsoft Office Specialist: Microsoft Office PowerPoint® 2013",
-    issuer: "Microsoft",
-    issuerIconName: "microsoft",
-    issueDate: "2017-04-16",
-    credentialID: "wXTm3-4SHG",
-    credentialURL:
-      "https://drive.google.com/file/d/117DE6bMkHvRqXbED2tmSMNkbx9s7OahM/view?usp=sharing",
-  },
-  {
-    title: "Google Code-in 2016",
-    issuer: "Google",
-    issuerIconName: "google",
-    issueDate: "2017-01-16",
+    title: "Scrum Fundamentals Certified (SFC)",
+    issuer: "ScrumStudy",
+    issuerIconName: "scrumstudy",
+    issuerLogoURL: "",
+    issueDate: "2024-05-01",
     credentialID: "",
-    credentialURL:
-      "https://drive.google.com/file/d/162RXtAVIZEvfx6LvP3xeBj-cSI9ZpPUX/view?usp=sharing",
+    credentialURL: "",
   },
   {
-    title: "HSGS Olympiad 2016",
-    issuer: "HUS High School for Gifted Students",
-    issueDate: "2016-05-08",
-    credentialID: "",
-    credentialURL:
-      "https://drive.google.com/file/d/1eKv-2ldfw8-hF27sKfKWXL8MmwcSkzPq/view?usp=sharing",
-  },
-  {
-    title: "Microsoft Office Specialist: Microsoft Office Word 2013",
-    issuer: "Microsoft",
-    issuerIconName: "microsoft",
-    issueDate: "2016-04-24",
-    credentialID: "w9YXr-FMR8",
-    credentialURL:
-      "https://drive.google.com/file/d/1-NHhjKlQbhlcO7bpRue1XzDgDaudOf2N/view?usp=sharing",
+    title: "Attendance Hashgraph Developer",
+    issuer: "The Hashgraph Association",
+    issuerIconName: "hashgraph",
+    issuerLogoURL: "",
+    issueDate: "2025-04-01",
+    credentialID: "a638d61c-32c0-4f43-90e1-17f14436d666",
+    credentialURL: "/certifications/hashgraph.pdf",
   },
 ];
